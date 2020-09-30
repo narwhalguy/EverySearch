bea6c81b888ab1fd3d2494ba6ec3b4d7c7525a04
narwhalguy <57877891+narwhalguy@users.noreply.github.com>
2020-09-30 10:34:09 -0700

    Complete overhaul of backend and frontend
    Added search engine selection and preference saving.

--- read.js
+++ read.js
@@ -1,18 +1,75 @@
-
-
+var browser;
 var i;
 var b;
 var a;
 var d;
 var e;
 var c;
+var bing=function(){
+  browser="https://www.bing.com/search?q=";
+localStorage.clear();
+ localStorage.setItem("browser", "https://www.bing.com/search?q=");
+}
+var wiki=function(){
+  browser="https://wikipedia.com/wiki/search?q=";
+localStorage.clear();
+localStorage.setItem("browser", "https://wikipedia.com/wiki/");
+}
+var giphy=function(){
+	browser="https://giphy.com/search/";
+	localStorage.clear();
+	localStorage.setItem("browser","https://giphy.com/search/")
+}
+var yahoo=function(){
+  browser="https://search.yahoo.com/search?p=";
+localStorage.clear();
+localStorage.setItem("browser", "https://search.yahoo.com/search?p=");
+}
+$(document).ready(function(){
+var header = $('body');
+
+var backgrounds = new Array(
+    'url(backdrop.jpg)'
+  , 'url(backdrop2.jpg)'
+  , 'url(backdrop4.jpg)'
+);
+
+var current = 0;
+$(document).keypress(function(event){
+var keycode = (event.keyCode ? event.keyCode : event.which);
+if(keycode == '49'){
+wiki();
+}});
+$(document).keypress(function(event){
+var keycode = (event.keyCode ? event.keyCode : event.which);
+if(keycode == '50'){
+bing();
+}});
+$(document).keypress(function(event){
+var keycode = (event.keyCode ? event.keyCode : event.which);
+if(keycode == '51'){
+yahoo();
+}});
+$(document).keypress(function(event){
+var keycode = (event.keyCode ? event.keyCode : event.which);
+if(keycode == '52'){
+giphy();
+}});
+function nextBackground() {
+    current++;
+    current = current % backgrounds.length;
+    header.css('background-image', backgrounds[current]);
+}
+setInterval(nextBackground, 15000);
+
+header.css('background-image', backgrounds[0]);
+});
 $(document).keypress(function(event){
 var keycode = (event.keyCode ? event.keyCode : event.which);
 if(keycode == '13'){
+     browser=localStorage.getItem("browser");
     i = document.getElementById("input").value;
-b="<iframe width='33%' height='100%' src='https://www.bing.com/search?q="+i+"'></iframe>";
-a="<iframe width='33%' height='100%' src='https://en.wikipedia.org/wiki/Special:Search?search="+i+"&go=Go&ns0=1'>;</iframe>"
-c="<iframe width='33%' height='100%' src='https://www.wolframalpha.com/input/?i="+i+"'></iframe>";
+c="<iframe width='100%' height='100%' src='"+browser+i+"'></iframe>";
 d="<style>body{background-color:black;}</style>"
 e="<head><title>Search Results</title></head>"
 document.body.innerHTML = '';
